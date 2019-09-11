4dc43d193d811e523310e04cebd2c287f55b6678
zlp <1131808958@qq.com>
2019-09-11 17:22:35 +0800

    常用js

--- pages/usercenter/usercenter.js
+++ pages/usercenter/usercenter.js
@@ -4,23 +4,93 @@
  * @version: 
  * @Date: 2019-09-03 09:41:43
  * @LastEditors: zlp
- * @LastEditTime: 2019-09-03 09:47:12
+ * @LastEditTime: 2019-09-11 17:00:01
  */
 // pages/usercenter/usercenter.js
+let utils = require("../../utils/util")
 Page({
 
     /**
      * 页面的初始数据
      */
     data: {
+        date: null,
+        formatDate: '',
+        formats: [
+            "yyyy-MM-dd hh:mm:ss",
+            "yyyy-MM-dd hh:mm",
+            "yyyy-MM-dd",
+            "yyyy-MM",
+            "yyyy/MM/dd hh:mm:ss",
+            "yyyy/MM/dd hh:mm",
+            "yyyy/MM/dd",
+            "yyyy/MM",
+            "yyyy年MM月dd日 hh时mm分ss秒",
+            "yyyy年MM月dd日 hh时mm分",
+            "yyyy年MM月dd日",
+        ],
+        index: 0
+    },
 
+    getInputValue(e) {
+        this.setData({
+            date: e.detail.value
+        })
+    },
+
+    bindchange(e) {
+        this.setData({
+            index: e.detail.value
+        })
+    },
+
+    format() {
+        let formatDate = utils.formatDate(this.data.date, this.data.formats[this.data.index])
+        this.setData({
+            formatDate
+        })
+        console.log(formatDate)
+    },
+
+    test() {
+        let timestamp = new Date().getTime()
+        let timestampFormats = []
+        let dateFormate = []
+            //时间戳转
+        this.data.formats.map(item => {
+                timestampFormats.push(utils.formatDate(timestamp, item))
+            })
+            //日期格式化
+        this.data.formats.map(item => {
+            dateFormate.push(utils.formatDate(new Date(), item))
+        })
+        console.log("<------- timestamp -------->", timestampFormats)
+        console.log("<------- date -------->", dateFormate)
+        console.log("<------- getDateAfter -------->", utils.formatDate(utils.getDate(3)))
+        console.log("<------- getDateBefore -------->", utils.formatDate(utils.getDate(-3)))
+        console.log("<------- formatMoney -------->", utils.formatMoney(2019091112))
+        console.log("<------- randomId -------->", utils.randomId(20))
+        console.log("<------- randomNum -------->", utils.randomNum(2, 20))
+        console.log("<------- checkDataType -------->", utils.checkDataType({}, 'Object'))
+        console.log("<------- isEmptyArray -------->", utils.isEmptyArray([]))
+        console.log("<------- isEmptyObject -------->", utils.isEmptyObject({}))
+        const random = {
+            randomNum() {
+                return Math.floor(Math.random() * 100)
+            },
+            randomArray() {
+                return Array.from(Array(utils.randomNum(10, 20)), _ => this.randomNum())
+            }
+        }
+        let array = random.randomArray()
+        console.log("<------- quickSort -------->", array, utils.quickSort(array))
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function(options) {
-
+        this.test()
     },
 
     /**
--- utils/util.js
+++ utils/util.js
@@ -1,19 +1,149 @@
+/*
+ * @Author: zlp
+ * @Description: 
+ * @version: 
+ * @Date: 2019-08-27 16:57:24
+ * @LastEditors: zlp
+ * @LastEditTime: 2019-09-11 17:07:14
+ */
 const formatTime = date => {
-  const year = date.getFullYear()
-  const month = date.getMonth() + 1
-  const day = date.getDate()
-  const hour = date.getHours()
-  const minute = date.getMinutes()
-  const second = date.getSeconds()
+    const year = date.getFullYear()
+    const month = date.getMonth() + 1
+    const day = date.getDate()
+    const hour = date.getHours()
+    const minute = date.getMinutes()
+    const second = date.getSeconds()
 
-  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+    return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
 }
 
 const formatNumber = n => {
-  n = n.toString()
-  return n[1] ? n : '0' + n
+    n = n.toString()
+    return n[1] ? n : '0' + n
 }
 
+/**
+ * 接收两个参数，
+ * @param {*} oldDate 日期(Date,String,Number)
+ * @param {*} fmt 格式(yyyy-MM-dd hh:mm)
+ */
+const formatDate = (oldDate, fmt = 'yyyy-MM-dd hh:mm') => {
+        let date = new Date()
+        if (typeof oldDate === 'string' || typeof oldDate === 'number') {
+            date = new Date(+oldDate)
+        } else {
+            date = oldDate
+        }
+        if (/(y+)/.test(fmt)) {
+            fmt = fmt.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length))
+        }
+        let o = {
+            'M+': date.getMonth() + 1,
+            'd+': date.getDate(),
+            'h+': date.getHours(),
+            'm+': date.getMinutes(),
+            's+': date.getSeconds()
+        }
+
+        const padLeftZero = (str) => {
+            return ('00' + str).substr(str.length)
+        }
+        for (let k in o) {
+            if (new RegExp(`(${k})`).test(fmt)) {
+                let str = o[k] + ''
+                fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? str : padLeftZero(str))
+            }
+        }
+        return fmt
+    }
+    /**
+     * 获取过去/未来num天的时间 过去：num为负数，未来：num为正数
+     * @param {*} num 
+     */
+const getDate = (num) => {
+    return Date.now() + num * 24 * 60 * 60 * 1000
+}
+
+/**
+ * 格式化金钱 
+ * @param {*} num 
+ * num..toLocaleString('en-US')同样可实现
+ */
+
+const formatMoney = num => num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+/**
+ * 生成随机id
+ */
+const randomId = len => Number(Math.random().toString().substr(3, len) + Date.now()).toString(36);
+
+/**
+ * 生成范围随机数
+ * @param {*} min 
+ * @param {*} max 
+ */
+const randomNum = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+/**
+ * 判断数据类型
+ * @param {*} data 
+ * @param {*} type 
+ */
+const checkDataType = (data, type) => {
+    let dataType = Object.prototype.toString.call(data).replace(/\[object /g, "").replace(/\]/g, "").toLowerCase();
+    let formatDataType = dataType.toLowerCase()
+    let currentType = type.toLowerCase()
+    return type ? formatDataType === currentType : dataType;
+
+}
+
+/**
+ * 判断是否为空数组
+ * @param {Array} array 
+ */
+const isEmptyArray = array => {
+    return Array.isArray(array) && !array.length;
+}
+
+/**
+ * 判断是否为空对象
+ * @param {Object} object 
+ */
+const isEmptyObject = object => {
+    return checkDataType(object, 'object') && !Object.keys(object).length
+}
+
+/**
+ * 快速排序
+ * @param {Array} array 
+ */
+const quickSort = arr => {
+    if (arr.length <= 1) { return arr; }　　
+    var pivotIndex = Math.floor(arr.length / 2);　　
+    var pivot = arr.splice(pivotIndex, 1)[0];　　
+    var left = [];　　
+    var right = [];　　
+    for (var i = 0; i < arr.length; i++) {　　　　
+        if (arr[i] < pivot) {　　　　　　
+            left.push(arr[i]);　　　　
+        } else {　　　　　　
+            right.push(arr[i]);　　　　
+        }　　
+    }
+    return [...quickSort(left), pivot, ...quickSort(right)]
+}
+
+
+
 module.exports = {
-  formatTime: formatTime
+    formatTime: formatTime,
+    formatDate,
+    getDate,
+    formatMoney,
+    randomId,
+    randomNum,
+    checkDataType,
+    isEmptyArray,
+    isEmptyObject,
+    quickSort
 }
